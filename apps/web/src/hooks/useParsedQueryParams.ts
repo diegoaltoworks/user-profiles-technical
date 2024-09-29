63bf109d4b7df4a6739013c6d432add7a1aa9a67
Diego Alto <diego@diegoalto.co.uk>
2024-09-29 16:18:35 +0100

    fix(schema): useQueryParams hook optioal schema

--- apps/web/src/hooks/useParsedQueryParams.ts
+++ apps/web/src/hooks/useParsedQueryParams.ts
@@ -1,7 +1,7 @@
 import { useSearchParams } from "next/navigation";
 import { ZodSchema } from "zod";
 
-export const useParsedQueryParams = <T>(schema: ZodSchema) => {
+export const useParsedQueryParams = <T>(schema?: ZodSchema) => {
   const searchParams = useSearchParams();
   const params: { [name: string]: string } = Object.fromEntries(
     searchParams.entries(),
